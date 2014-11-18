a956ffcc7bfc250af3270e79d097e85360144f65
Lennart Kats <lclkats@gmail.com>
2014-11-18 08:27:02 +0100

    Revert "Merge pull request +5022 from c9/update-ace-all"
    This reverts commit bf407bff971f927effe77841735571dfff5584ae, reversing
    changes made to eb12e2bd2aecd11ddc5831b90747cbad4a8a21f1.

--- HTMLInstrumentation.js
+++ HTMLInstrumentation.js
@@ -205,7 +205,7 @@ define(function (require, exports, module) {
         if (text && !isDangerousEdit(text)) {
             // If the edit is right at the beginning or end of a tag, we want to be conservative
             // and use the parent as the edit range.
-            var startNode = _getNodeAtDocumentPos(session, delta.start, true);
+            var startNode = _getNodeAtDocumentPos(session, delta.range.start, true);
             if (startNode) {
                 var range = Range.fromPoints(startNode.startPos, startNode.endPos);
                 if (range) {
@@ -492,8 +492,8 @@ define(function (require, exports, module) {
     }
     
     function updatePositions(dom, delta) {
-        var start = delta.start;
-        var end = delta.end;
+        var start = delta.range.start;
+        var end = delta.range.end;
         var sign = delta.action[0] == "i" ? 1 : -1;
         var rowChange = sign * (end.row - start.row);
         var columnChange = sign * (end.column - start.column);
@@ -754,18 +754,18 @@ define(function (require, exports, module) {
             }
             else if (change[0] == -1) {
                 deltas.push({
-                    start: {row: row, column: column},
-                    end: {row: endRow, column: endColumn},
-                    lines: lines,
-                    action: "remove"
+                    range: new Range(row, column, endRow, endColumn),
+                    lines: rowCh && lines,
+                    text: !rowCh && text,
+                    action: rowCh ? "removeLines" : "removeText"
                 });
             }
             else if (change[0] == 1) {
                 deltas.push({
-                    start: {row: row, column: column},
-                    end: {row: endRow, column: endColumn},
-                    lines: lines,
-                    action: "insert"
+                    range: new Range(row, column, endRow, endColumn),
+                    lines: rowCh && lines,
+                    text: !rowCh && text,
+                    action: rowCh ? "insertLines" : "insertText"
                 });
                 row = endRow;
                 column = endColumn;
